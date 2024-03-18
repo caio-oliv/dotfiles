bc1cba04754a4b16d8363b2018a8613d56f70e8a
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2024-03-17 21:43:19 -0300

    document how to clear uneeded packages

--- .config/sys/package/backup-pkglist.sh
+++ .config/sys/package/backup-pkglist.sh
@@ -2,6 +2,12 @@
 
 set -e;
 
+echo "NOTE: Try clearing uneeded packages first for a small package list";
+echo "";
+echo "Arch wiki guides:";
+echo "- https://wiki.archlinux.org/title/Pacman/Tips_and_tricks#Removing_unused_packages_(orphans)";
+echo "- https://wiki.archlinux.org/title/Pacman/Tips_and_tricks#Detecting_more_unneeded_packages";
+
 explicit_path=explicit-pkglist.txt;
 foreign_path=foreign-pkglist.txt;
 
