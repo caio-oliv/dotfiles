b2e6785b16043742c5f2059f6d3e1e8ee4189127
Caio Oliveira <caio.vsoliveira23@gmail.com>
2024-07-18 11:27:32 -0300

    update mimetypes and chromium config flags

--- .config/zsh/history_opt.zsh
+++ .config/zsh/history_opt.zsh
@@ -11,7 +11,7 @@ setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming 
 setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
 setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
 setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
-setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
+setopt HIST_IGNORE_SPACE         # Do not record a command starting with a space.
 setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
 setopt APPEND_HISTORY            # append to history file
