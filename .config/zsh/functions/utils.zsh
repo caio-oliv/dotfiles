7cf1e68d829a683b49541af00b630eb52f831b4f
Caio Oliveira <caio.vsoliveira23@gmail.com>
2024-11-27 12:00:32 -0300

    fix chrome flags, add vscode settings

--- .config/zsh/functions/utils.zsh
+++ .config/zsh/functions/utils.zsh
@@ -2,5 +2,5 @@ function todofind() {
   local invalid_around="[^_\"'a-zA-Z]";
   local regex="$invalid_around(TODO|NOTE|TEST|FIX|FIXME|PERF|SAFETY)($invalid_around:?)";
 
-  rg $regex;
+  rg $regex $@;
 }
