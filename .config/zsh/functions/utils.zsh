e61bb8204ab10442e05500d5129ac82e0c28ce31
Caio Oliveira <caio.vsoliveira23@gmail.com>
2025-09-13 11:49:29 -0300

    bluetooth policy, updated hyprland config and others

--- .config/zsh/functions/utils.zsh
+++ .config/zsh/functions/utils.zsh
@@ -5,10 +5,29 @@ function todofind() {
   rg $regex $@;
 }
 
+function random-string() {
+  local str=$(cat /dev/urandom | base64 | head -c $1);
+  echo "$str";
+}
+
 function git-conflict-accept-both() {
   local regex="^(=======)|(<<<<<<<*)|(>>>>>>>*)";
 
   rg --invert-match $regex $@;
+}
+
+function git-semantic-msg() {
+  echo -n "- feat: adds or remove features
+- fix: fixes a bug
+- refactor: rewrite/restructure code
+- perf: special refactor commits that improve performance
+- style: do not affect any meaning (white-space, formatting, semi-colons, etc)
+- test: add missing tests, corrects or remove existing tests
+- docs: affect only documentation
+- build: affect build componentes (build tools, ci pipeline, dependencies, project version, etc)
+- ops: operational componentes (infrastructure, deployment, backup, recovery)
+- chore: miscellaneos
+";
 }
 
 function reload-bluetooth() {
