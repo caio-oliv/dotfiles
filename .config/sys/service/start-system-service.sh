89f7ff7ad16d64125305b7a0109192ef4f738a67
Caio Oliveira <caio.vsoliveira23@gmail.com>
2025-11-30 16:46:52 -0300

    enable SMART system service

--- .config/sys/service/start-system-service.sh
+++ .config/sys/service/start-system-service.sh
@@ -8,3 +8,7 @@ sudo systemctl enable --now seatd;
 # ssh daemon
 # See: https://wiki.archlinux.org/title/OpenSSH#Daemon_management
 sudo systemctl enable --now sshd;
+
+# SMART status monitor
+# See: https://wiki.archlinux.org/title/S.M.A.R.T.#daemon_management
+sudo systemctl enable --now smartd;
